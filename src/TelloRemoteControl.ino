dab1acdf7d37b674559d2005cb00e34acf6b7681
Matan Brightbert <matana0000@gmail.com>
2024-02-02 20:31:42 +0200

    Code refactoring

--- src/TelloRemoteControl.ino
+++ src/TelloRemoteControl.ino
@@ -1,78 +1,89 @@
-#include <cstdint>
 #include <WiFi.h>
 #include <WiFiUdp.h>
 
-const char* const SSID = "TELLO-9F5E9A";
-const char* const PASSWORD = "";
 
-const char* const TELLO_IP = "192.168.10.1";
-const uint32_t TELLO_PORT = 8889;
+// Debug Messages
+const String MESSAGE__FIRMWARE_INFO = "Tello Remote-Controller | v0.1";
+const String STATUS_MESSAGE__CONNECTING_TO_TELLO_WIFI = "Connecting to Tello's Wi-Fi... ";
+const String STATUS_MESSAGE__INITIALIZING_CONNECTION_TO_TELLO = "Initializing a connection to Tello... ";
+const String STATUS_MESSAGE__ENABLING_SDK_MODE = "Enabling SDK mode... ";
+const String STATUS_MESSAGE_RESPONSE__SUCCESS = "SUCCESS!";
+const String STATUS_MESSAGE_RESPONSE__FAIL = "FAIL!";
+// Connections parameters constants
+const String TELLO_WIFI_SSID = "TELLO-9F5E9A";
+const String TELLO_WIFI_PASSWORD = "";
+const String TELLO_IP = "192.168.10.1";
+const int TELLO_PORT = 8889;
+const int LOCAL_PORT = 9000;    // Local port to bind the UDP socket to. Can be any free port.
 
-const uint32_t MAX_PACKET_SIZE = 255;
-char tello_packet[MAX_PACKET_SIZE];
+bool initialized = false;
+byte buffer[1000];
+WiFiUDP tello_connection;
 
-WiFiUDP udp_connection;
+
+void initialize()
+{
+    Serial.begin(9600);
+    Serial.println(MESSAGE__FIRMWARE_INFO);
+}
+
+void connect_to_wifi()
+{
+    Serial.print(STATUS_MESSAGE__CONNECTING_TO_TELLO_WIFI);
+    WiFi.begin(TELLO_WIFI_SSID.c_str(), TELLO_WIFI_PASSWORD.c_str());
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(1);
+    }
+    Serial.println(STATUS_MESSAGE_RESPONSE__SUCCESS);
+}
+
+bool initialize_connection_to_tello()
+{
+    Serial.print(STATUS_MESSAGE__INITIALIZING_CONNECTION_TO_TELLO);
+    bool succeeded = tello_connection.begin(LOCAL_PORT);
+    Serial.println(succeeded ? STATUS_MESSAGE_RESPONSE__SUCCESS : STATUS_MESSAGE_RESPONSE__FAIL);
+    return succeeded;
+}
+
+bool enable_sdk_mode()
+{
+    Serial.print(STATUS_MESSAGE__ENABLING_SDK_MODE);
+    tello_connection.beginPacket(TELLO_IP.c_str(), TELLO_PORT);
+    String("command").getBytes(buffer, sizeof(buffer) / sizeof(buffer[0]));
+    tello_connection.write(buffer, sizeof(buffer) / sizeof(buffer[0]));
+    bool succeeded = tello_connection.endPacket();
+    Serial.println(succeeded ? STATUS_MESSAGE_RESPONSE__SUCCESS : STATUS_MESSAGE_RESPONSE__FAIL);
+    return succeeded;
+}
+
 
 void setup()
 {
-    Serial.begin(115200);
-    delay(1000);
-
-    WiFi.begin(SSID, PASSWORD);
-    Serial.println("Connecting to TELLO's Wi-Fi");
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        delay(500);
-        Serial.print(".");
-    }
-
-    Serial.println("\nConnected!");
-
-    udp_connection.begin(TELLO_PORT);
-
-    const String command = "command";
-
-    byte buffer[command.length() + 1];
-    command.getBytes(buffer, sizeof(buffer) / sizeof(buffer[0]));
-    
-    udp_connection.write(buffer, sizeof(buffer) / sizeof(buffer[0]));
+    delay(5000);
+    initialize();
 
     delay(1000);
+    connect_to_wifi();
 
-    const String command2 = "takeoff";
-
-    byte buffer2[command2.length() + 1];
-    command2.getBytes(buffer2, sizeof(buffer2) / sizeof(buffer2[0]));
-    
-    udp_connection.write(buffer2, sizeof(buffer2) / sizeof(buffer2[0]));
-
-    bool read_packet_succeeded = false;
-    
-    while (!read_packet_succeeded)
+    delay(1000);
+    if (!initialize_connection_to_tello())
     {
-        int32_t packetSize = udp_connection.parsePacket();
-
-        Serial.println("Packet size: ");
-        Serial.println(packetSize);
-        
-        if (packetSize != 0)
-        {
-            int32_t len = udp_connection.read(tello_packet, MAX_PACKET_SIZE);
-            
-            if (len > 0)
-            {
-                tello_packet[len] = 0;
-            }
-
-            Serial.printf("UDP packet contents: %s\n", tello_packet);
-            
-            read_packet_succeeded = true;
-        } 
+        return;
     }
 
+    delay(1000);
+    if (!enable_sdk_mode())
+    {
+        return;
+    }
+
+    initialized = true;
 }
 
 void loop()
 {
-
+    if (initialized) {
+        // TODO: ...
+    }
 }
